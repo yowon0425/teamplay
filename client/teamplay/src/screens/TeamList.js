f21213007aa3cea3fff123e9737507123b67ba89
채요원 <email address>
2023-09-03 20:17:26 +0900

    4-팀페이지_UI_생성

--- client/teamplay/src/screens/TeamList.js
+++ client/teamplay/src/screens/TeamList.js
@@ -1,12 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import { View, Text, TouchableOpacity, ScrollView, Image, SafeAreaView, StyleSheet } from 'react-native';
 
 const TeamList = () => {
   return (
-    <View>
-      <Text>TeamList</Text>
-    </View>
-  )
+    <>
+      <LinearGradient colors={['#FFB8D0', '#FEE5E1']} 
+        style={styles.linearGradient}
+      >
+        <View style={styles.textContainer}>
+          <Text style={styles.teamName}>
+            팀 이름
+          </Text>
+        </View>
+        <Text style={styles.teamDescription}>
+          팀 소개
+        </Text>
+      </LinearGradient>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.greyBox}>
+          <Text style={styles.plusText}>+</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    width: '90%',
+    height: '25%',
+    top: '10%',
+    left: '5%',
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    alignItems: 'center', 
+  },
+  teamName: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  teamDescription: {
+    marginTop: 10,
+    color: 'black',
+  },
+  greyBox: {
+    width: '90%',
+    height: '45%',
+    top: '40%',
+    left: '5%',
+    backgroundColor: '#BDBDBD',
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  plusText: {
+    fontSize: 100,
+    bottom: '20%',
+    color: 'black',
+  },
+});
 
 export default TeamList;
