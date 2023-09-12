1b9844b89e348671cd359ae02e0b48bd640efc50
JIMIN1020 <jimins4920@gmail.com>
2023-09-12 22:00:14 +0900

    16-firebase 구글 로그인 구현

--- client/teamplay/App.js
+++ client/teamplay/App.js
@@ -1,35 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import Home from './src/screens/Home';
-import Work from './src/screens/Work';
-import Notice from './src/screens/Notice';
-import Calender from './src/screens/Calender';
-import Message from './src/screens/Message';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import TeamList from './src/screens/TeamList';
+
 import Main from './src/screens/Main';
+import ScreenNavigator from './src/ScreenNavigator';
 
 const App = () => {
-  const stack = createNativeStackNavigator();
-  const Tab = createBottomTabNavigator();
-  const Navigator = () => {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen name="Work" component={Work} />
-        <Tab.Screen name="Notice" component={Notice} />
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Calender" component={Calender} />
-        <Tab.Screen name="Message" component={Message} />
-      </Tab.Navigator>
-    );
-  };
-  return (
-    <View>
-      <Main />
-      {/* <TeamList /> */}
-    </View>
-  );
+  return <ScreenNavigator />;
 };
 
 export default App;
--- client/teamplay/src/screens/Main.js
+++ client/teamplay/src/screens/Main.js
@@ -1,46 +1,14 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import axios from 'axios';
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
+import GoogleLogin from '../components/GoogleLogin';
 
 const Main = () => {
-  const [idToken, setIdToken] = useState(undefined);
-
-  useEffect(() => {
-    googleConfig = GoogleSignin.configure({
-      webClientId:
-        '790378980309-58chnk7o3c9fes7r1e4vbpnp69dn1ta2.apps.googleusercontent.com',
-    });
-  }, []);
-
-  /* ---------- 구글 로그인 ---------- */
-  const onPressLogin = async () => {
-    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-    const {idToken} = await GoogleSignin.signIn();
-    console.log('idToekn : ', idToken);
-    if (idToken) {
-      setIdToken(idToken);
-    }
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    const res = await auth().signInWithCredential(googleCredential);
-    console.log('ok');
-  };
-
   return (
     <LinearGradient style={styles.background} colors={['#033495', '#AEE4FF']}>
       <View style={styles.container}>
         <Text style={styles.title}>TeamPlay</Text>
-        <TouchableOpacity onPress={onPressLogin}>
-          <View style={styles.button}>
-            <Image
-              source={require('../../assets/images/google.png')}
-              style={styles.logo}
-            />
-            <Text style={styles.text}>Google로 시작하기</Text>
-          </View>
-        </TouchableOpacity>
+        <GoogleLogin />
       </View>
     </LinearGradient>
   );
@@ -63,22 +31,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
     marginBottom: 150,
-  },
-  button: {
-    flexDirection: 'row',
-    width: 250,
-    height: 40,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  logo: {
-    width: 20,
-    height: 20,
-  },
-  text: {
-    fontSize: 16,
-    color: 'black',
   },
 });
