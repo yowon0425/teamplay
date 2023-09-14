a4f9d586d4ac589250eba2287960b3be5274f168
Kim Eun Yeong <iris578@naver.com>
2023-09-14 21:36:25 +0900

    10-홈 UI 생성

--- client/teamplay/src/components/Button.js
+++ client/teamplay/src/components/Button.js
@@ -2,7 +2,7 @@ import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {Shadow} from 'react-native-shadow-2';
 
-const Button = ({text, light}) => {
+const Button = ({text, light, onPress}) => {
   /* 
   text prop은 버튼 안에 써 있는 문구를 받아옴
   light는 버튼이 누를 수 있는 상태인지 아닌지 볼 수 있음
@@ -13,14 +13,19 @@ const Button = ({text, light}) => {
     <View>
       {light ? (
         <Shadow style={styles.shadow}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor: '#FAE6E2'}]}
+            onPress={onPress}>
             <Text style={styles.text}>{text}</Text>
           </TouchableOpacity>
         </Shadow>
       ) : (
-        <TouchableOpacity>
-          <Text>{text}</Text>
-        </TouchableOpacity>
+        <Shadow style={styles.shadow}>
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor: '#F1F1F1'}]}>
+            <Text style={styles.text}>{text}</Text>
+          </TouchableOpacity>
+        </Shadow>
       )}
     </View>
   );
@@ -32,7 +37,6 @@ const styles = StyleSheet.create({
   button: {
     width: 160,
     height: 45,
-    backgroundColor: '#FAE6E2',
     borderRadius: 20,
     borderColor: 'black',
     borderWidth: 1,
--- client/teamplay/src/screens/Home.js
+++ client/teamplay/src/screens/Home.js
@@ -1,12 +1,128 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, ProgressBarAndroidBase, StyleSheet} from 'react-native';
+import React from 'react';
+import MenuBar from '../components/MenuBar';
+import {NavigationContainer} from '@react-navigation/native';
+import * as Progress from 'react-native-progress';
+import LinearGradient from 'react-native-linear-gradient';
+import Button from '../components/Button';
 
-const Home = () => {
+const Home = ({teamName, goal}) => {
+  const textGradientStyle = {
+    fontSize: 20,
+    color: 'white',
+    background: '-webkit-linear-gradient(#6A9CFD, #FEE5E1)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  };
+
+  const percent = 100;
   return (
     <View>
-      <Text>Home</Text>
+      <View style={styles.top}>
+        <Text style={styles.teamName}>{teamName}팀이름</Text>
+        <Text style={styles.goal}>{goal}목표는 세계정복</Text>
+      </View>
+      <View style={styles.teamProgress}>
+        <LinearGradient
+          style={styles.mainPercent}
+          colors={['#6A9CFD', '#FEE5E1']}>
+          <Text style={styles.percentText}>{percent}%</Text>
+        </LinearGradient>
+        <Progress.Bar style={styles.mainBar} />
+        <Text style={styles.progressText}>Project Progress...</Text>
+      </View>
+      <View style={styles.members}>
+        <View style={styles.member}>
+          <View style={styles.memberInfo}>
+            <Text>이름(역할)</Text>
+            <Text>퍼센트</Text>
+          </View>
+          <Progress.Bar style={styles.memberBar} />
+        </View>
+        <View style={styles.member}>
+          <View style={styles.memberInfo}>
+            <Text>이름(역할)</Text>
+            <Text>퍼센트</Text>
+          </View>
+          <Progress.Bar style={styles.memberBar} />
+        </View>
+      </View>
+      <View style={{alignItems: 'center', margin: 20}}>
+        <Button text="내 작업 페이지로" light={true} />
+      </View>
+      <NavigationContainer style={{alignItems: 'flex-end'}}>
+        <MenuBar />
+      </NavigationContainer>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+const styles = StyleSheet.create({
+  top: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  teamName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    margin: 10,
+  },
+  goal: {
+    fontSize: 16,
+    color: 'black',
+  },
+  teamProgress: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainPercent: {
+    fontSize: 96,
+    fontWeight: '900',
+    color: 'white',
+    background: '-webkit-linear-gradient(#6A9CFD, #FEE5E1)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  },
+  percentText: {
+    fontSize: 96,
+    fontWeight: '900',
+    color: 'rgba(0,0,0,0.05)',
+  },
+  mainBar: {
+    width: '80%',
+    height: 30,
+    borderRadius: 30,
+    borderColor: 'black',
+    unfilledColor: 'gray',
+  },
+  progressText: {
+    margin: 10,
+    fontSize: 20,
+    color: 'black',
+  },
+  members: {
+    alignItems: 'center',
+    margin: 40,
+    width: '100%',
+  },
+  member: {
+    width: '100%',
+    margin: 10,
+  },
+  memberInfo: {
+    flexDirection: 'row',
+    width: '80%',
+    justifyContent: 'space-between',
+  },
+  memberBar: {
+    width: '80%',
+    height: 10,
+    borderRadius: 20,
+    borderColor: 'black',
+    unfilledColor: 'gray',
+  },
+});
