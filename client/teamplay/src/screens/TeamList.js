6f2bde45139378509a9da3bd7ab6d5e22fd1c227
채요원 <email address>
2023-09-30 15:46:34 +0900

    30 - 팀리스트 기능 보완

--- client/teamplay/src/screens/TeamList.js
+++ client/teamplay/src/screens/TeamList.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import { View, Text, TouchableOpacity, ScrollView, Image, SafeAreaView, StyleSheet } from 'react-native';
+import Button from '../components/Button';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 
 const TeamList = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const toggleOptions = () => {
+    setShowOptions(!showOptions);
+  };
+
   return (
-    <>
-      <LinearGradient colors={['#FFB8D0', '#FEE5E1']} 
-        style={styles.linearGradient}
-      >
+    <SafeAreaView style={styles.container}>
+      <LinearGradient colors={['#FFB8D0', '#FEE5E1']} style={styles.linearGradient}>
         <View style={styles.textContainer}>
           <Text style={styles.teamName}>
             팀 이름
@@ -17,27 +22,28 @@ const TeamList = () => {
           팀 소개
         </Text>
       </LinearGradient>
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.greyBox}>
-          <Text style={styles.plusText}>+</Text>
-        </View>
-      </SafeAreaView>
-    </>
+      <TouchableOpacity onPress={toggleOptions} style={styles.buttonContainer}>
+        <Button style={[styles.input, { width: '90%' }]} text="+" light={false} />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   linearGradient: {
     width: '90%',
-    height: '25%',
-    top: '10%',
-    left: '5%',
+    height: '15%',
     borderRadius: 25,
     alignItems: 'center',
     justifyContent: 'center',
   },
   textContainer: {
-    alignItems: 'center', 
+    alignItems: 'center',
   },
   teamName: {
     fontSize: 30,
@@ -48,19 +54,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
     color: 'black',
   },
-  greyBox: {
-    width: '90%',
-    height: '45%',
-    top: '40%',
-    left: '5%',
-    backgroundColor: '#BDBDBD',
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
+  buttonContainer: {
+    marginTop: 20,
   },
-  plusText: {
-    fontSize: 100,
-    bottom: '20%',
+  optionsContainer: {
+    marginTop: 10,
+  },
+  optionText: {
+    fontSize: 16,
     color: 'black',
   },
 });
