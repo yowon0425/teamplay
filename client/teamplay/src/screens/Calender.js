4c8503051c3a911bae4e33ba0d400d8a70ac3cd9
채요원 <email address>
2023-11-01 22:49:11 +0900

    40 달력 기능 보완

--- client/teamplay/src/screens/Calender.js
+++ client/teamplay/src/screens/Calender.js
@@ -1,36 +1,36 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import {LocaleConfig} from 'react-native-calendars';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native'; // Import TouchableOpacity
+import { Calendar } from 'react-native-calendars';
+import { LocaleConfig } from 'react-native-calendars';
 
 LocaleConfig.locales['fr'] = {
   monthNames: [
-    'Janvier',
-    'Février',
-    'Mars',
-    'Avril',
-    'Mai',
-    'Juin',
-    'Juillet',
-    'Août',
-    'Septembre',
-    'Octobre',
-    'Novembre',
-    'Décembre',
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
   ],
   monthNamesShort: [
-    'Janv.',
-    'Févr.',
-    'Mars',
-    'Avril',
-    'Mai',
-    'Juin',
-    'Juil.',
-    'Août',
-    'Sept.',
-    'Oct.',
-    'Nov.',
-    'Déc.',
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
   ],
   dayNames: [
     '일요일',
@@ -42,57 +42,38 @@ LocaleConfig.locales['fr'] = {
     '토요일',
   ],
   dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-  today: "Aujourd'hui",
+  today: "오늘",
 };
 LocaleConfig.defaultLocale = 'fr';
 
 class Calender extends Component {
   render() {
+    const currentDate = new Date();
+    const currentDateString = currentDate.toISOString().split('T')[0];
+
     return (
-      <View style={{paddingTop: 50, flex: 1}}>
+      <View style={{ paddingTop: 50, flex: 1, justifyContent: 'space-between' }}>
         <Calendar
-        // Initially visible month. Default = Date()
-        current={'2023-10-17'}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={'2020-01-01'}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={'2024-12-31'}
-        // Handler which gets executed on day press. Default = undefined
-        onDayPress={(day) => {console.log('selected day', day)}}
-        // Handler which gets executed on day long press. Default = undefined
-        onDayLongPress={(day) => {console.log('selected day', day)}}
-        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-        monthFormat={'yyyy MM'}
-        // Handler which gets executed when visible month changes in calendar. Default = undefined
-        onMonthChange={(month) => {console.log('month changed', month)}}
-        // Hide month navigation arrows. Default = false
-        hideArrows={true}
-        // Replace default arrows with custom ones (direction can be 'left' or 'right')
-        renderArrow={(direction) => (<Arrow/>)}
-        // Do not show days of other months in month page. Default = false
-        hideExtraDays={true}
-        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-        // day from another month that is visible in calendar page. Default = false
-        disableMonthChange={true}
-        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-        firstDay={1}
-        // Hide day names. Default = false
-        hideDayNames={false}
-        // Show week numbers to the left. Default = false
-        showWeekNumbers={false}
-        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-        onPressArrowLeft={substractMonth => substractMonth()}
-        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-        onPressArrowRight={addMonth => addMonth()}
-        // Disable left arrow. Default = false
-        disableArrowLeft={true}
-        // Disable right arrow. Default = false
-        disableArrowRight={true}
-        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-        disableAllTouchEventsForDisabledDays={true}
-        /** Replace default month and year title with custom one. the function receive a date as parameter. */
-        //renderHeader={(date) => {/*Return JSX*/}}
+          current={currentDateString}
+          monthFormat={'yyyy년 MM월'}
         />
+        <View style={{ position: 'absolute', top: 450, right: 10 }}>
+          <TouchableOpacity
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 20,
+              backgroundColor: 'gray',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              // Handle the button press here
+            }}
+          >
+            <Text style={{ fontSize: 24, color: 'white' }}>+</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
